2f8d77f4f3fa978c910e07bec48d36bc0508cb31
Jens Meindertsma <mail@jensmeindertsma.com>
2025-05-31 17:12:15 +0200

    Inline and database use

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,68 @@
 import type { Route } from "./+types/home";
+import { getDatabase } from "~/services/database.server";
+import { useEffect, useRef } from "react";
+import { Form, redirect, useNavigation } from "react-router";
 
 export function meta(): Route.MetaDescriptors {
   return [{ title: "Home" }];
 }
 
-export default function Home() {
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (navigation.state === "submitting") {
+      formRef.current?.reset();
+    }
+  }, [navigation.state]);
+
   return (
     <>
       <title>Home</title>
+
       <h1>Home</h1>
+      <p>We currently have {loaderData.users.length} users!</p>
+
+      <Form method="post" ref={formRef}>
+        <input
+          type="text"
+          name="name"
+          required
+          placeholder="Name the user..."
+        />
+        <button type="submit">Create User</button>
+      </Form>
+
+      <h2>Users</h2>
+      <ul>
+        {loaderData.users.map((user) => (
+          <li key={user.id}>
+            ID={user.id} NAME={user.name}
+          </li>
+        ))}
+      </ul>
     </>
   );
+}
+
+export async function loader() {
+  const database = getDatabase();
+
+  return { users: await database.user.findMany() };
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const name = formData.get("name");
+
+  if (typeof name != "string") {
+    throw new Response("Invalid form data!", { status: 400 });
+  }
+
+  const database = getDatabase();
+
+  await database.user.create({ data: { name } });
+
+  return redirect("/");
 }
--- app/services/database.server.ts
+++ app/services/database.server.ts
@@ -2,14 +2,8 @@ import { PrismaClient } from "../generated/prisma";
 import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 
 export function getDatabase() {
-  const databaseUrl = process.env.APPLICATION_DATABASE_URL;
-
-  if (!databaseUrl) {
-    throw new Error("Missing `APPLICATION_DATABASE_URL`");
-  }
-
   const adapter = new PrismaBetterSQLite3({
-    url: databaseUrl,
+    url: "file:./data/database.db",
   });
 
   const client = new PrismaClient({ adapter });
